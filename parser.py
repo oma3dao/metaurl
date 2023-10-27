4884f9ba94ae73f95d04d7defac14b2657730e7a
Joel Dietz <joel@swarm.com>
2023-10-27 14:42:46 +0200

    Update parser.py
    had some inaccuracies

--- parser.py
+++ parser.py
@@ -1,16 +1,19 @@
 import re
 
 def is_valid_metaurl(metaurl):
-    # Define a regular expression pattern to match the format
-    pattern = r'^/(-?\d+(\.\d+){2,3}/)+$'
+    # Define a regular expression pattern to match the format with or without trailing slash
+    pattern = r'^(\d+(\.\d+)+/)*\d+(\.\d+)+/?$'
     
     # Use the re.match function to check if the string matches the pattern
     return bool(re.match(pattern, metaurl))
 
 # Test cases
-print(is_valid_metaurl('/3.4.3/3.4.3/'))  # Valid
-print(is_valid_metaurl('/-3.4.3/3.4.3/'))  # Valid
-print(is_valid_metaurl('/3.4/3.4/'))  # Valid (two dots)
-print(is_valid_metaurl('/3.4.3/3.4/'))  # Invalid (mismatched number of coordinates)
-print(is_valid_metaurl('/3.4.3/3..4/'))  # Invalid (extra dot)
-print(is_valid_metaurl('/3.4.3/3.4.3/4.5.6/'))  # Invalid (extra coordinate)
+print(is_valid_metaurl('/3.4.4'))  # Valid
+print(is_valid_metaurl('/3.4.4/'))  # Valid (with trailing slash)
+print(is_valid_metaurl('/5.3.5/3.4.3'))  # Valid
+print(is_valid_metaurl('/4.5.100/3.14.4/3.4.5'))  # Valid
+
+# Invalid examples
+print(is_valid_metaurl('/-3.4.4'))  # Invalid (negative integer)
+print(is_valid_metaurl('/3.4.4/5.3.5/3.4.3'))  # Invalid (no trailing slash)
+print(is_valid_metaurl('/3.4.4//3.4.4/'))  # Invalid (double slashes)
